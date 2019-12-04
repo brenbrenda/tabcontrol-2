1465254a0ae121f9a1154b709c5c209c14535a44
CHIYA <chia930429@gmail.com>
2019-12-04 10:42:41 +0800

    Initial Commit

---
+++ tabcontrol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabcontrol
+//
+//  Created by chia on 2019/12/4.
+//  Copyright © 2019 chia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
